7ffb6ea0f46f25d842347a0c11d8a45a65492045
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-04-18 11:34:54 +0600

    Added Trapezoid, updated reqs.txt, json-tests

--- backend/APP/urls.py
+++ backend/APP/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [    
-    path('diff/', diff_list),
-    path('taylor/', taylor_list),
-    path('newton/', newton_list),
-    path('simpson/', simpson_list),
-    path('trapezoid/', trapezoid_list),
+    path('diff/', diff_list),                      #Abbosjon
+    path('taylor/', taylor_list),                  #Elnazar
+    path('newton/', newton_list),                  #Abbosjon
+    path('simpson/', simpson_list),                #Elnazar
+    path('trapezoid/', trapezoid_list),            #Ilkhom
 ]
