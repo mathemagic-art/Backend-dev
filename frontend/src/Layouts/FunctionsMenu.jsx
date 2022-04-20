49fc78feba6d8e95f3c79f21dd7c5205e00d6e66
marzia <marzia.tabanjafari_2025@uca.corp>
2022-04-20 23:39:53 +0500

    Functions menu added to frontend

---
+++ frontend/src/Layouts/FunctionsMenu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import RouteItem from "../Components/RouteItem";
+
+
+
+const FunctionsMenu = () => {
+
+return (
+
+<>
+
+<ul className="w-[30rem] h-full bg-black opacity-90 absolute left-0 top-24">
+
+<RouteItem text="Newton's Method Calculator" path="Newton" />
+
+</ul>
+
+</>
+
+);
+
+};
+
+
+
+export default FunctionsMenu;
