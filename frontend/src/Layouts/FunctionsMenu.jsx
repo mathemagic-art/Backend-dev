497f83a4e2da65baf417a62dd12d75dc627a9dee
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-04-21 00:59:00 +0600

    updated home

--- frontend/src/Layouts/FunctionsMenu.jsx
+++ frontend/src/Layouts/FunctionsMenu.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import RouteItem from "../Components/RouteItem";
 
 const FunctionsMenu = () => {
@@ -13,31 +12,3 @@ const FunctionsMenu = () => {
 };
 
 export default FunctionsMenu;
-=======
-
-import RouteItem from "../Components/RouteItem";
-
-
-
-const FunctionsMenu = () => {
-
-return (
-
-<>
-
-<ul className="w-[30rem] h-full bg-black opacity-90 absolute left-0 top-24">
-
-<RouteItem text="Newton's Method Calculator" path="Newton" />
-
-</ul>
-
-</>
-
-);
-
-};
-
-
-
-export default FunctionsMenu;
->>>>>>> 9c8a80e2450ee2f41e85e4f57faf68f5b80967eb
