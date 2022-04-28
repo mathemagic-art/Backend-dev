46179c4c255219c60e8379d3ef2d1fd0a32c1cea
oneku16 <eku.ulanov@yandex.com>
2022-04-28 19:34:52 +0600

    general updates

--- backend/APP/serializers.py
+++ backend/APP/serializers.py
@@ -32,14 +32,14 @@ class String_Float_Float_Integer(serializers.Serializer):
     arg_4_int = serializers.IntegerField()
 
 
-class String(serializers.Serializer):
-
-    arg_1_str = serializers.CharField(max_length=300)
-
 class String_String_String_String(serializers.Serializer):
     
     arg_1_str = serializers.CharField(max_length=300)
     arg_2_str = serializers.CharField(max_length=300)
     arg_3_str = serializers.CharField(max_length=300)
     arg_4_str = serializers.CharField(max_length=300)
-       
+
+
+class String_(serializers.Serializer):
+
+    arg_1_str = serializers.CharField(max_length=300)
