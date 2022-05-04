214eccf12fb6349afca75c9415e74136bef442e9
sharukhhhan <shodavlat21@gmail.com>
2022-05-05 01:02:58 +0600

    added diff generator

--- backend/APP/mathematics.py
+++ backend/APP/mathematics.py
@@ -1,5 +1,6 @@
 from re import search, findall
 from math import factorial
+from random import choice, randint
 from numpy import linspace, random, var
 from sympy import Symbol, latex, parse_expr, sympify, lambdify, diff, Float, limit, integrate, calculus, S
 from scipy import integrate as scipy_integrate
@@ -320,4 +321,56 @@ def universal_integral(type: str, function: str, variable: str, initial_point: f
         ans = integrate(function, variable)
         return output_func(ans)
 
+#########################################################################################################################
 
+def generateDifferentiation(level=2):
+
+    # dependencies
+    problem = ''
+    types = {
+        'Trigonometric': ['sin', 'cos', 'tan', 'cot', 'sec'],
+        'Exponential': ['e'],
+        'Logarithmic': ['ln']
+    }
+    operators = ["+", "-", "*", "/"]
+    random_operator = choice(operators)
+
+    # layouts
+    def polynomial():
+        coeff = randint(2, 5)
+        power = randint(2, 5)
+        problem = "{}*x**{}".format(coeff, power)
+        return problem
+
+    def trigonometric():
+        coeff_1 = randint(2, 5)
+        coeff_2 = randint(2, 5)
+        func_type = list(types.keys())[0]
+        func = choice(types[func_type])
+        problem = "({}*{}*({}*x))".format(coeff_1, func, coeff_2)
+        return problem
+
+    # logic
+    if level == 1:
+        problem += "({}){}({})".format(polynomial(),
+                                       random_operator, polynomial())
+
+    if level == 2:
+        problem += "({}){}({})".format(trigonometric(),
+                                       random_operator, trigonometric())
+
+    if level == 3:
+        coeff_1 = randint(2, 5)
+        coeff_2 = randint(2, 5)
+        power_1 = randint(2, 5)
+        func_type = choice(list(types.keys())[1:])
+        func = types[func_type][0]
+
+        if func == 'e':
+            problem += "{}*{}**({}*x**{})".format(
+                polynomial(), func, coeff_2, power_1)
+        else:
+            problem += "{}*{}*({}){}{}".format(
+                coeff_1, func, polynomial(), random_operator, trigonometric())
+
+    return problem
