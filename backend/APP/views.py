42ab9721d1cdd5c8210b1c4a2c1e7443b89f1678
oneku16 <eku.ulanov@yandex.com>
2022-05-05 18:07:19 +0600

    added test_geneartors.py

--- backend/APP/views.py
+++ backend/APP/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
-from .mathematics import *
+from .calculators import *
 
 
 @api_view(['POST'])
